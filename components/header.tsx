aa8cc71bb3f83335bb2aefb98bdcb5b765f7beba
0xgmcarl <126638250+crafty-arl@users.noreply.github.com>
2024-11-06 19:25:31 -0600

    remove env header

--- components/header.tsx
+++ components/header.tsx
@@ -50,7 +50,6 @@ export function HeaderComponent() {
           <div className="flex items-center space-x-2">
             <Pen className="h-5 w-5 text-orange-200" />
             <h1 className="text-base font-semibold text-orange-100">WriteMyNewsletter</h1>
-            <p className="text-sm text-orange-100">Thirdweb Client ID: {process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}</p>
           </div>
           <nav className="hidden md:flex space-x-6">
             <Link className="text-pink-200 hover:text-orange-200 transition-colors text-sm" href="/">
